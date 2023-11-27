6cbafec43c22d5df68d51c09b0b21eb1571f3379
CarlosCo_Dev <condorpasa26@gmail.com>
2023-11-26 23:39:19 -0500

    solve erros asideright

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -4,12 +4,14 @@ import Landing from "../components/landing/landing";
 import DashboardPatient from "../components/dashboards/patient/dashboardPatient";
 import DashboardMaster from "../components/dashboards/master/dashboardMaster";
 import DashboardDoctor from "../components/dashboards/doctor/dashboardDoctor";
+import UserForm from "../components/userForm/userForm";
 
 
 
 const router = createBrowserRouter([
   {path:'/',element:<Landing/>},
   {path:'/patient',element:<DashboardPatient/>},
+  {path:'/patientForm',element:<UserForm/>},
   {path:'/patient/detailDoctor/:id',element:<p>holi</p>},
   {path:'/master',element:<DashboardMaster/>},
   {path:'/doctor',element:<DashboardDoctor/>},
