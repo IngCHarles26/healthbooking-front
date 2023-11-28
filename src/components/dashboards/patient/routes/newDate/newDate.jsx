25f754ff7410657076afadc3024cb7d4671e31a4
Santiago Paz <santipaz19.02@gmail.com>
2023-11-27 21:59:34 -0300

    Pasarela de pagos
    .

--- src/components/dashboards/patient/routes/newDate/newDate.jsx
+++ src/components/dashboards/patient/routes/newDate/newDate.jsx
@@ -6,8 +6,15 @@ import searchDoctor from '../../../../assets/brands/search-doctor.svg';
 import leftArrow from '../../../../assets/brands/arrow-left-newDate.svg';
 import rightArrow from '../../../../assets/brands/arrow-right-newDate.svg';
 
+import axios from "axios";
+
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+
+// MERCADOPAGO
+import { initMercadoPago, Wallet } from '@mercadopago/sdk-react'
+
+
 
 
 const cardsPerPage = 14;
@@ -26,49 +33,59 @@ const routes = {
 }
 
 function NewDate(props) {
-  let {sures,doctors,specialtys} = props;
+  let { sures, doctors, specialtys } = props;
   const [filtSure, setFiltSure] = useState('');
   const [filtSpecialty, setFiltSpecialty] = useState('');
   const [filtName, setFiltName] = useState('');
   const [orderName, setOrderName] = useState('');
 
-  const [maxPage, setMaxPage] = useState(Math.ceil(doctors.length/cardsPerPage));
+  const [maxPage, setMaxPage] = useState(Math.ceil(doctors.length / cardsPerPage));
   const [currentPage, setCurrentPage] = useState(1);
 
   const [doctorSelected, setDoctorSelected] = useState(0);
+  const [preference, setPreference] = useState({ id: "", price: "", name: "" });
 
-  // console.log({doctorSelected})
+  initMercadoPago('TEST-a343d129-f780-4d77-8de3-0cbccf82c334');
 
-  const filterDoctors = ()=>{
+
+
+
+  const filterDoctors = () => {
     let aux = [...doctors]
-    if(filtSpecialty) aux = [...aux].filter(doctor=>+doctor.Specialty === +filtSpecialty);
-    if(filtSure) aux = [...aux].filter(doctor=>doctor.Sures.includes(+filtSure));
-    aux = [...aux].filter(doctor=>doctor.name.toLowerCase().includes(filtName.toLowerCase()));
-    if(orderName) {
+    if (filtSpecialty) aux = [...aux].filter(doctor => +doctor.Specialty === +filtSpecialty);
+    if (filtSure) aux = [...aux].filter(doctor => doctor.Sures.includes(+filtSure));
+    aux = [...aux].filter(doctor => doctor.name.toLowerCase().includes(filtName.toLowerCase()));
+    if (orderName) {
       aux = [...aux].sort((a, b) =>
         orderName === "a-z"
           ? a.name.localeCompare(b.name)
           : b.name.localeCompare(a.name)
       );
-    } 
-    let maxPages = Math.ceil(aux.length/cardsPerPage);
-    if(!maxPages) maxPages = 1;
-    if(maxPage != maxPages) setMaxPage(maxPages);
-    if(currentPage>maxPages) setCurrentPage(1)
+    }
+    let maxPages = Math.ceil(aux.length / cardsPerPage);
+    if (!maxPages) maxPages = 1;
+    if (maxPage != maxPages) setMaxPage(maxPages);
+    if (currentPage > maxPages) setCurrentPage(1)
     // console.log({currentPage,maxPage})
     return aux;
   }
   const filtered = filterDoctors()
-  const handlePage = (one)=>{
-    let newPage = currentPage+one;
-    if(0<newPage && newPage<=maxPage) setCurrentPage(newPage);
+  const handlePage = (one) => {
+    let newPage = currentPage + one;
+    if (0 < newPage && newPage <= maxPage) setCurrentPage(newPage);
   }
 
-  const handleSendInfo = ()=>{
-    //Enviar la informacion del doctor (variable doctorSelected)
+  const handleSendInfo = async (buyDate) => {
+    const response = await axios.post(
+      "http://localhost:3001/pay",
+      buyDate
+    );
+    console.log(response)
+
+    window.location.href = response.data;
   }
 
-  return ( 
+  return (
     <main>
       <header>Dashboard &#62; Agendar Cita</header>
 
@@ -76,15 +93,15 @@ function NewDate(props) {
         <header>Agendar cita</header>
 
         <section className="select-doctor">
-          <header>        
+          <header>
             <h1>Seleccione el doctor de su preferencia:</h1>
 
             <ul className="tabs-group">
-              {specialtys.slice(1).map((el,ix)=>
-                <SpecialtyOption 
-                  key={'spec_'+ix}
-                  name={el} 
-                  index={ix+1} 
+              {specialtys.slice(1).map((el, ix) =>
+                <SpecialtyOption
+                  key={'spec_' + ix}
+                  name={el}
+                  index={ix + 1}
                   filtSpecialty={filtSpecialty}
                   setFiltSpecialty={setFiltSpecialty}
                 />)}
@@ -92,22 +109,22 @@ function NewDate(props) {
 
             <div className="filters-group">
               <div className="options">
-                <div onClick={()=>setOrderName('')}>{/*agregar aqui el evento on Click */}
+                <div onClick={() => setOrderName('')}>{/*agregar aqui el evento on Click */}
                   <img src={deleteOrder} alt="delete " />
                 </div>
 
                 <form>
                   <label htmlFor="order">Sort <img src={orderCards} alt="orderCards" /></label>
-                  <select id="order" value={orderName} onChange={(e)=>{e.preventDefault();setOrderName(e.target.value)}}>
-                      <option value="a-z">Ascendente</option>
-                      <option value="z-a">Descendente</option>
+                  <select id="order" value={orderName} onChange={(e) => { e.preventDefault(); setOrderName(e.target.value) }}>
+                    <option value="a-z">Ascendente</option>
+                    <option value="z-a">Descendente</option>
                   </select>
 
                 </form>
               </div>
 
               <form className="search-bar" id="searh-in-grid">
-                <input type="text" value={filtName} onChange={(e)=>{e.preventDefault();setFiltName(e.target.value)}}/>
+                <input type="text" value={filtName} onChange={(e) => { e.preventDefault(); setFiltName(e.target.value) }} />
                 <div className="submit">
                   <img src={searchDoctor} alt="searchDoctor" />
                 </div>
@@ -116,36 +133,38 @@ function NewDate(props) {
               <div className="options">
                 <form >
                   <label htmlFor="order">Obra Social <img src={orderCards} alt="orderCards" /></label>
-                  <select name="" id="order" value={filtSure} onChange={()=>{}}>
-                    {sures.slice(1).map((el,ix)=>
-                      <ObraSocial 
-                        key={'sure_'+ix}
-                        name={el} 
-                        ix={ix+1} 
+                  <select name="" id="order" value={filtSure} onChange={() => { }}>
+                    {sures.slice(1).map((el, ix) =>
+                      <ObraSocial
+                        key={'sure_' + ix}
+                        name={el}
+                        ix={ix + 1}
                         setFiltSure={setFiltSure}
                       />)}
                   </select>
                 </form>
-                <div onClick={()=>setFiltSure('')}>{/*agregar aqui el evento on Click */}
+                <div onClick={() => setFiltSure('')}>{/*agregar aqui el evento on Click */}
                   <img src={deleteFilter} alt="delete " />
                 </div>
               </div>
             </div>
           </header>
-          
+
           <form action="">
             <fieldset className="select-doctor">
               <section className="cards-grid">
-                {filterDoctors().slice( (currentPage-1)*cardsPerPage , currentPage*cardsPerPage ).map(doctor=>
-                  <DoctorCard 
-                    key={'doctor-card-'+doctor.id}
-                    image={doctor.profilePicture} 
-                    name={doctor.name} 
+                {filterDoctors().slice((currentPage - 1) * cardsPerPage, currentPage * cardsPerPage).map(doctor =>
+                  <DoctorCard
+                    key={'doctor-card-' + doctor.id}
+                    image={doctor.profilePicture}
+                    name={doctor.name}
                     specialty={specialtys[doctor.Specialty]}
                     id={doctor.id}
+                    price={doctor.price}
                     doctorSelected={doctorSelected}
                     setDoctorSelected={setDoctorSelected}
-                    />)}
+                    setPreference={setPreference}
+                  />)}
               </section>
             </fieldset>
           </form>
@@ -154,13 +173,13 @@ function NewDate(props) {
             <nav aria-label='Navegación por páginas'>
               <ul className="pagination">
                 <li className="page-item">
-                  <a href="" className="prev" onClick={(e)=>{e.preventDefault();handlePage(-1)}}>
+                  <a href="" className="prev" onClick={(e) => { e.preventDefault(); handlePage(-1) }}>
                     <img src={leftArrow} alt="leftArrow" />
                   </a>
                 </li>
-                <p className="pageButton">{filtered.length ? currentPage+' de '+maxPage : 'Sin coincidencias'}</p>
+                <p className="pageButton">{filtered.length ? currentPage + ' de ' + maxPage : 'Sin coincidencias'}</p>
                 <li className="page-item">
-                  <a href="" className="prev" onClick={(e)=>{e.preventDefault();handlePage(+1)}}>
+                  <a href="" className="prev" onClick={(e) => { e.preventDefault(); handlePage(+1) }}>
                     <img src={rightArrow} alt="rightArrow" />
                   </a>
                 </li>
@@ -171,7 +190,10 @@ function NewDate(props) {
       </article>
 
       <section>
-        <button onClick={()=>{handleSendInfo}}>Generar Cita</button>
+        {/* <Wallet initialization={{ preferenceId: preference.id }} /> */}
+        {/* {console.log(preference)} */}
+        <button className="btnBuy" onClick={() => { handleSendInfo(preference) }}>Generar Cita</button>
+
       </section>
     </main>
   );
@@ -181,47 +203,59 @@ export default NewDate;
 
 
 function SpecialtyOption(props) {
-  let {name,index,setFiltSpecialty,filtSpecialty} = props;
-  let style = 'tab'+(index==filtSpecialty ? ' specialty-selected' : '');
-  return ( 
-    <li className={style} onClick={()=>setFiltSpecialty(index == filtSpecialty ? '' : index)}>
+  let { name, index, setFiltSpecialty, filtSpecialty } = props;
+  let style = 'tab' + (index == filtSpecialty ? ' specialty-selected' : '');
+  return (
+    <li className={style} onClick={() => setFiltSpecialty(index == filtSpecialty ? '' : index)}>
       {name}
     </li>
   );
 }
 
 function ObraSocial(props) {
-  let {name,setFiltSure,ix} = props
-  return ( 
-    <option onClick={()=>setFiltSure(ix)}>
+  let { name, setFiltSure, ix } = props
+  return (
+    <option onClick={() => setFiltSure(ix)}>
       {name}
     </option>
   );
 }
 
 function NumberPage(props) {
-  let {number} = props;
-  return ( 
+  let { number } = props;
+  return (
     <li className="page-item">
       <a href="">{number}</a>
     </li>
   );
 }
 
-function DoctorCard(props){
-  let {image,id,name,specialty,doctorSelected,setDoctorSelected} = props;
+function DoctorCard(props) {
+  let { image, id, name, specialty, price, doctorSelected, setDoctorSelected, setPreference } = props;
+
+  const handleDoctorSelection = () => {
+    setDoctorSelected(id == doctorSelected ? 0 : id);
+
+    setPreference({
+      id: id,
+      price: price,
+      name: name,
+      image: image
+    });
+  };
 
   const navigate = useNavigate();
 
-  let style = 'tile'+(id==doctorSelected ? ' doctor-selected' : '');
-  return(
+  let style = 'tile' + (id == doctorSelected ? ' doctor-selected' : '');
+  return (
     <article className={style}>
-      <img src={image} alt="imageDoctor" onClick={()=>{navigate(routes.getDetail+id)}}/>
-      <section className="card-description" onClick={()=>{setDoctorSelected(id == doctorSelected ? 0 : id)}}>
+      <img src={image} alt="imageDoctor" onClick={() => { navigate(routes.getDetail + id) }} />
+      <section className="card-description" onClick={() => { handleDoctorSelection(price, id, name, image) }}>
         <h1>{name}</h1>
         <p className="specialization">{specialty}</p>
         <p className="licence">{id}</p>
-      </section>
-    </article>
+        <p className="specialization">${price}</p>
+      </section >
+    </article >
   )
 }
