8327d3e12d7061e797d97a960e1582991adc11b4
1W3111990ChaparroSantiago <69601377+1W3111990ChaparroSantiago@users.noreply.github.com>
2023-12-11 15:36:41 -0300

    componentes para graficos

--- src/components/dashboards/master/routes/HistorialPagos/HistorialPagos.jsx
+++ src/components/dashboards/master/routes/HistorialPagos/HistorialPagos.jsx
@@ -1,56 +1,32 @@
-/*
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer, BarChart, Bar } from 'recharts';
-import { useEffect } from 'react';
-import { useState } from 'react';
+
+
+
+import { useEffect, useState } from 'react';
 import axios from 'axios';
-import leftArrow from "../../../../assets/brands/left-arrow.svg";
-import rightArrow from '../../../../assets/brands/right-arrow.svg'
-
-
+import leftArrow from '../../../../assets/brands/left-arrow.svg';
+import rightArrow from '../../../../assets/brands/right-arrow.svg';
 
 const HistorialPagos = () => {
- const [data,setData] = useState([]);
- const [totalAmountPerSpecialty, setTotalAmountPerSpecialty] = useState([]);
+  const [data, setData] = useState([]);
 
-    useEffect(()=>{
-        try {
-            const fetchAppointments  = async() => {
-                const response = await axios.get('http://localhost:3001/master/appointment');
 
-                //const url = 'https://healtbooking-backend.onrender.com/master/appointment'
-                const filterData = response.data.filter(cita => cita.status === "pending")
-                setData(filterData);
-                console.log(response.data)
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('http://localhost:3001/master/appointment');
+        const filterData = response.data.filter(cita => cita.status === 'pending');
+        setData(filterData);
 
-                const chartData = {};
-                data.forEach(cita => {
-                    const specialty = cita.Doctor.Specialty.name;
-                    const amount = cita.finalAmount;
-                    //  console.log(cita.specialty)
-                    if(chartData[specialty]){
-                        chartData[specialty] += amount;
-                    }else{
-                        
-                        chartData[specialty] = amount;
-                    }
-                });
-                const obj = Object.entries(chartData).map(([name, value]) => ({ name, value }));
-                
-                //console.log(obj)
-                await setTotalAmountPerSpecialty(obj)
-                console.log(JSON.stringify(totalAmountPerSpecialty))
+      } catch (error) {
+        console.error({ message: 'Error al cargar los datos', error });
+      }
+    };
+    fetchData();
+  }, [data]);
 
-         }
-         fetchAppointments();
-        } catch (error) {
-            console.error({message:'Error al cargar las citas',error})
-        };
-           
-    },[totalAmountPerSpecialty])
-
-  const [currentPage, setCurrentPage] = useState(1)
-  const [perPage, setPerPage] = useState(5)
-  const max = Math.ceil(data.length / perPage)
+  const [currentPage, setCurrentPage] = useState(1);
+  const [perPage, setPerPage] = useState(5);
+  const max = Math.ceil(data.length / perPage);
 
   const next = () => {
     setCurrentPage(currentPage + 1);
@@ -59,19 +35,13 @@ const HistorialPagos = () => {
     setCurrentPage(currentPage - 1);
   };
 
-  const formatDate = (dateString) => {
-    const options = { year: 'numeric', month: '2-digit', day: '2-digit' };
-    const formattedDate = new Date(dateString).toLocaleDateString(undefined, options);
-    return formattedDate;
-  };
-  
 
   return (
-    <main>
+    <main >
       <header>Dashboard &#62; Home</header>
 
       <article className="summary">
-        <header>Historial de pagos</header>
+        <header >Historial de pagos</header>
 
         <article className="table-wrapper">
           <table>
@@ -84,7 +54,6 @@ const HistorialPagos = () => {
               </tr>
             </thead>
             <tbody>
-
               {data?.slice(
                 (currentPage - 1) * perPage,
                 (currentPage - 1) * perPage + perPage
@@ -93,13 +62,12 @@ const HistorialPagos = () => {
                   <td>Pago turno médico</td>
                   <td>{cita.finalAmount}</td>
                   <td>{cita.status}</td>
-                  <td>{formatDate(cita.createdAt)}</td>
+                  <td>{cita.paymentDay}</td>
                 </tr>
               ))}
             </tbody>
           </table>
         </article>
-
       </article>
 
       <footer className="navigation">
@@ -112,90 +80,10 @@ const HistorialPagos = () => {
         <button disabled={currentPage === max} onClick={next}>
           <img src={rightArrow} alt="rightArrow" />
         </button>
-
       </footer>
-      <ResponsiveContainer width="100%" aspect={2}>
-        <BarChart 
-            data={totalAmountPerSpecialty} 
-            width={500} 
-            height={300} 
-            margin={{
-                top:5,
-                right:30,
-                left:20,
-                bottom:5
-            }}>
-            <CartesianGrid strokeDasharray="4 1 2"/>
-            <XAxis dataKey="name" fill='#6b48ff'/>
-            <YAxis/>
-            <Tooltip/>
-            <Legend/>
-            <Bar dataKey="value" fill='#1ee3cf'/>
-
-        </BarChart>
-      </ResponsiveContainer>
-
+     
     </main>
-    
   );
-
-
-};
-export default HistorialPagos;
-*/
-
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer, BarChart, Bar } from 'recharts';
-import { useEffect } from 'react';
-import { useState } from 'react';
-import { startOfWeek, endOfWeek, isWithinInterval } from 'date-fns';
-
-import axios from 'axios';
-
-const HistorialPagos = () => {
-
-    const [data,setData] = useState([]);
-
-    useEffect(()=>{
-        const fetchData = async()=>{
-
-           try {
-            
-            const response = await axios.get('http://localhost:3001/master/appointment');
-            
-            setData(response.data);
-           
-
-           } catch (error) {
-            console.error('Error al obtener los datos',error);
-           }
-          
-            
-        }
-        fetchData(); 
-    },[])
-  
-    const sumasPorSemana = {};
-
-    const formatDate = (dateString) => {
-      const options = { year: 'numeric', month: '2-digit', day: '2-digit' };
-      const formattedDate = new Date(dateString).toLocaleDateString(undefined, options);
-      return formattedDate;
-    };
-
-    data.forEach(cita => {
-      const fechaPago = formatDate(cita.createdAt)
-      console.log(cita.createdAt)
-    })
-    
-      return (
-        <LineChart width={600} height={300} data={sumasPorSemana}>
-          <XAxis dataKey="semana" />
-          <YAxis />
-          <Tooltip />
-          <Legend />
-          <Line type="monotone" dataKey="monto" stroke="#8884d8" />
-        </LineChart>
-      );
 };
 
 export default HistorialPagos;
